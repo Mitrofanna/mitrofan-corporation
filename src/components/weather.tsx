26afc376da161dc62d318d75a0f246edda5a8059
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-02-28 23:20:35 +0300

    выводит данные о погоде

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,24 +1,32 @@
+import { useState } from "react";
+
 const KEY_WEATHER = '6ee1dd5dfd29377c817bd1a70ad363de';
 
 function Weather(): JSX.Element {
-
-    async function getWeather(event: any) {
-        event.preventDefault();
-        const city = event.target.elements.city.value;
-
-        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY_WEATHER}`);
-        const result = await api.json();
-        console.log(result);
+    const [search, setSearch] = useState('');
+    const [weather, setWeather] = useState<any>({});
+    
+    function onClickSearchWeather() {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${KEY_WEATHER}`)
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result)
+                setWeather(result)});
     }
 
     return (
     <article className="weather promo__block promo__block--barbershop">
-        <form onSubmit={getWeather}>
-            <input className="weather__input" type="text" name="city" placeholder="город"></input>
-            <button className="weather__button" type="submit">узнать погоду</button>
+        <form>
+            <input className="weather__input" type="text" placeholder="город" onChange={(event) => setSearch(event.target.value)}></input>
+            <button className="weather__button" type="submit" onClick={onClickSearchWeather}>узнать погоду</button>
         </form>
-        <h3 className="weather__city">{}</h3>
-        <span className="weather__temperature">{}</span>
+        {weather.name !== undefined ? (
+            <div>
+                <h3 className="weather__city">{weather.name}</h3>
+                <p className="weather__temperature">{weather.main.temp}</p>
+                <p className="weather__condition">{weather.weather[0].main}</p>
+            </div>
+        ) : null}
     </article> 
     );
 }
