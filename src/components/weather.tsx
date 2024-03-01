1e499d9ee5f543f6bd89ce0ddb504a09fa102d36
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-03-01 21:47:29 +0300

    добавляет стили погоде, переводит градусы в цельсии

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles/weather.css";
 
 const KEY_WEATHER = '6ee1dd5dfd29377c817bd1a70ad363de';
 
@@ -6,7 +7,8 @@ function Weather(): JSX.Element {
     const [search, setSearch] = useState('');
     const [weather, setWeather] = useState<any>({});
     
-    function onClickSearchWeather() {
+    function onClickSearchWeather(e: any) {
+        e.preventDefault();
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${KEY_WEATHER}`)
             .then(response => response.json())
             .then((result) => {
@@ -15,15 +17,21 @@ function Weather(): JSX.Element {
     }
 
     return (
-    <article className="weather promo__block promo__block--barbershop">
-        <form>
-            <input className="weather__input" type="text" placeholder="город" onChange={(event) => setSearch(event.target.value)}></input>
-            <button className="weather__button" type="submit" onClick={onClickSearchWeather}>узнать погоду</button>
+    <article className="weather promo__block bg-pan-tr">
+        <form className="weather__form header__search">
+            <label className="visually-hidden">Введите свой город</label>
+            <input className="weather__input header__search-input form__input" type="text" placeholder="город" onChange={(event) => setSearch(event.target.value)}></input>
+            <button className="weather__button header__search-button button" type="submit" onClick={onClickSearchWeather}>
+            <svg className="weather__icon header__search-icon" width="18" height="18">
+                <use xlinkHref="#icon-search"></use>
+              </svg>
+              <span className="visually-hidden">Начать поиск</span>
+            </button>
         </form>
         {weather.name !== undefined ? (
-            <div>
+            <div className="weather__info">
                 <h3 className="weather__city">{weather.name}</h3>
-                <p className="weather__temperature">{weather.main.temp}</p>
+                <p className="weather__temperature">{Math.round(weather.main.temp - 273)}°C</p>
                 <p className="weather__condition">{weather.weather[0].main}</p>
             </div>
         ) : null}
