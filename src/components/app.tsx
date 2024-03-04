2a8750ba3fbc3ba22f11cd6e035277106195183d
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-03-04 14:56:02 +0300

    ограничивает доступ для неавторизованных

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import MainPage from "../pages/main-page";
-import { AppRoute } from "../const";
+import { AppRoute, AuthorizationStatus } from "../const";
 import RegistrationPage from "../pages/registration-page";
 import NotFoundPage from "../pages/not-found-page";
 import FeedPage from "../pages/feed-page";
 import ProfilePage from "../pages/profile-page";
 import MessagesPage from "../pages/messages-page";
 import PopularPage from "../pages/popular-page";
+import PrivateRoute from "./private-route";
 
 function App(): JSX.Element {
     return (
@@ -24,7 +25,10 @@ function App(): JSX.Element {
             />
             <Route
                 path={AppRoute.Feed}
-                element={<FeedPage />}
+                element={
+                    <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+                        <FeedPage />
+                    </PrivateRoute>}
             />          
             <Route
                 path={AppRoute.Profile}
